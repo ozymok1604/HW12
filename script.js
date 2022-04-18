7e4127778bf1e64e696e0d780b4304e9438a9e33
ozymok1604 <ozimok1604ua@gmail.com>
2022-04-18 14:07:29 +0300

    ss

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 function* idMaker() {
-  console.log(1);
-  yield 1;
-  console.log(2);
-  yield 2;
-  console.log(3);
-  yield 3;
-  console.log(4);
-  yield 4;
-  console.log(5);
+    let id  = 1;
+    while (true){
+        yield id;
+        id ++;
+    }
+    
+
+  
 }
 
 const id = idMaker();
-console.log(id.next());
-console.log(id.next());
-console.log(id.next());
-console.log(id.next());
-console.log(id.next());
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
 
-function* fontGenerator(size) {
-  let font = size;
-  while (true) {
-    var value = yield null;
-    console.log(value);
-    if (value == "up") {
-      console.log((font += 2));
-    }
-    if (value == "down") {
-      console.log((font -= 2));
-    }
-    if (value == "") {
-      console.log(font);
-    }
-  }
-}
+// function* fontGenerator(size) {
+//   let font = size;
+//   while (true) {
+//     var value = yield null;
+//     console.log(value);
+//     if (value == "up") {
+//       console.log((font += 2));
+//     }
+//     if (value == "down") {
+//       console.log((font -= 2));
+//     }
+//     if (value == "") {
+//       console.log(font);
+//     }
+//   }
+// }
 
-var g = fontGenerator(12);
-g.next(1);
+// var g = fontGenerator(12);
+// g.next(1);
 
-g.next("up");
-g.next("up");
-g.next("up");
+// g.next("up");
+// g.next("up");
+// g.next("up");
 
-g.next("down");
-g.next("down");
-g.next("down");
+// g.next("down");
+// g.next("down");
+// g.next("down");
 
-g.next("");
+// g.next("");
